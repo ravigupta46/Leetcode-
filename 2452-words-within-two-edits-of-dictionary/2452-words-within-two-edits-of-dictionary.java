ee75a200ff0dffc8debf17a966477ffe55b118c2
Ravi Gupta <ravig8370083@gmail.com>
2026-04-22 19:16:07 +0530

    Time: 6 ms (72.79%), Space: 44.1 MB (92.65%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> ans=new ArrayList<>();
+        
+        for(int i=0;i<queries.length;i++){
+            String key=queries[i];
+            for(int j=0;j<dictionary.length;j++){
+                String key2=dictionary[j];
+                int k=0;
+                int count=0;
+                while(k<key.length()){
+                    if(key.charAt(k)!=key2.charAt(k)){
+                        if(count>2) break;
+                        count++;
+                    }
+                    k++;
+                }
+                if(count<=2){
+                    ans.add(key);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
