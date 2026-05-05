6fde270a0915ace1c9d0d8c7a124816e95cd8350
Ravi Gupta <ravig8370083@gmail.com>
2026-05-05 11:02:34 +0530

    Time: 0 ms (100%), Space: 8.6 MB (70.35%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+       long long binform=n;
+       double ans=1.0;
+
+
+       if(n<0){
+        x=1/x;
+        binform =-binform;
+       }
+       while(binform>0){
+        if(binform%2==1){
+            ans=ans*x;
+        }
+        x=x*x;
+        binform=binform/2;
+       }
+       
+return ans;
+       
+    }
+};
