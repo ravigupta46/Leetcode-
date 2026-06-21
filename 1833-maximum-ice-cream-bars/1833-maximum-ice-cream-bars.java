9695d9aa039f2577676598128db46f861d4c41be
Ravi Gupta <ravig8370083@gmail.com>
2026-06-21 12:31:48 +0530

    Time: 9 ms (81.71%), Space: 83.1 MB (83.57%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,6 +1,26 @@
 class Solution {
+    public void countsort(int[] nums){
+        int maxi=0;
+        for(int a:nums){
+            maxi=Math.max(maxi,a);
+        }
+        int [] arr=new int[maxi+1];
+
+        for(int a:nums){
+            arr[a]++;
+        }
+        int idx=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                while(arr[i]>0){
+                    nums[idx++]=i;
+                    arr[i]--;
+                }
+            }
+        }
+    }
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
+       countsort(costs);
         int count=0;
         for(int a:costs){
             if(coins>=a){
