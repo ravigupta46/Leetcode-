9087f43362828e6f562f0e57a4f2f2a39cdfb070
Ravi Gupta <ravig8370083@gmail.com>
2026-07-06 19:05:40 +0530

    Time: 26 ms (5.09%), Space: 46.5 MB (62.65%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -3,7 +3,7 @@ class Solution {
 
         Arrays.sort(intervals, (a, b) -> {
             if (a[0] == b[0])
-                return a[0] - b[0];
+                return a[1] - b[1];
             return a[0] - b[0];
         });
 
